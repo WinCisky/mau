65dc3d915b65e3f2d2f7f018ea6b926835bc3863
Simone <xsimone97@hotmail.it>
2023-08-03 00:26:16 +0200

    fix url path

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script>
+    import { base } from '$app/paths';
     import "../app.css";
 </script>
 
 <div class="navbar bg-base-300">
     <div class="flex-1">
-        <a class="btn btn-ghost normal-case text-xl" href="/">Mau</a>
+        <a class="btn btn-ghost normal-case text-xl" href="{base}/">Mau</a>
     </div>
     <div class="flex-none">
     </div>
