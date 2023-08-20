186ef5ae45990cd563a27f1ec575e79481471f2c
Simone <xsimone97@hotmail.it>
2023-08-20 22:00:29 +0200

    github oauth

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,10 @@
     let searchText = "";
     let searchResults: Anime[] = [];
 
+    async function saveUserData() {
+        const userId = pb.authStore.model?.id;
+    }
+
     async function searchAnime() {
         const resultList = await pb.collection("mau_anime").getList(1, 20, {
             filter: `title_eng ~ '${searchText}' || title ~ '${searchText}'`,
@@ -160,11 +164,51 @@
                     <span class="label-text">Mirror</span>
                 </label>
             </li>
+
+            <h2 class="text-2xl font-bold my-6 mt-auto">
+                {pb.authStore.isValid && pb.authStore.model?.username
+                    ? pb.authStore.model?.username.length > 19
+                        ? pb.authStore.model?.username.substring(0, 19) + "..."
+                        : pb.authStore.model?.username
+                    : "Account"}
+            </h2>
+            <button
+                class="btn btn-outline"
+                on:click={async () => {
+                    if (pb.authStore.isValid) {
+                        pb.authStore.clear();
+                    } else {
+                        await pb
+                            .collection("users")
+                            .authWithOAuth2({ provider: "github" });
+                        if (pb.authStore.isValid) {
+                            saveUserData();
+                        }
+                    }
+                    // console.log(pb.authStore);
+                    window.location.href = `${base}/`;
+                }}
+            >
+                {pb.authStore.isValid ? "Logout" : "Login"}
+                {#if !pb.authStore.isValid}
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-6 w-6 fill-current"
+                        viewBox="0 0 100 100"
+                        stroke="currentColor"
+                        ><path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
+                        /></svg
+                    >
+                {/if}
+            </button>
         </ul>
     </div>
 
-    <dialog id="my_modal_search" class="modal">
-        <form method="dialog" class="modal-box w-fit max-w-none">
+    <dialog id="my_modal_search" class="modal modal-top md:modal-middle">
+        <form method="dialog" class="modal-box w-full md:w-fit max-w-none">
             <div class="flex flex-col justify-center items-center">
                 <div>
                     <input
@@ -179,13 +223,18 @@
                         class="mt-6 h-[60vh] md:max-w-[80vw] md:min-w-[40vw] flex flex-wrap justify-center gap-4 md:gap-8 pb-10 overflow-y-auto"
                     >
                         {#each searchResults as result}
+                            <!-- TODO: fix url with proper anime link -->
                             <a
                                 class="card w-36 md:w-52 max-h-72 bg-base-100 shadow-xl image-full"
                                 href={`${base}/player/${result.slug}/1`}
                                 target="_self"
                             >
                                 <figure>
-                                    <img src={result.imageurl} alt="Cover" class="w-full" />
+                                    <img
+                                        src={result.imageurl}
+                                        alt="Cover"
+                                        class="w-full"
+                                    />
                                 </figure>
                                 <div class="card-body w-36 md:w-52">
                                     <h2 class="card-title">
