d62c7a382240b0b9e48dd97a25d7141c4d06e2d9
Simone <xsimone97@hotmail.it>
2023-08-20 16:39:12 +0200

    search

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,6 +2,10 @@
 
 export function decodeHTMLEntities(str: string | undefined) {
     if (str === undefined) return "";
+    //check if DOMParser is available
+    if (typeof DOMParser === "undefined") {
+        return str;
+    }
     var doc = new DOMParser().parseFromString(str, "text/html");
     return doc.documentElement.textContent;
 }
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,30 @@
 <script lang="ts">
+    import PocketBase from "pocketbase";
     import { base } from "$app/paths";
     import { onMount } from "svelte";
+    import type { Anime } from "$lib/db_helper";
     import "../app.css";
+
+    const pb = new PocketBase("https://dev.opentrust.it/");
+
+    let searchText = "";
+    let searchResults: Anime[] = [];
+
+    async function searchAnime() {
+        const resultList = await pb.collection("mau_anime").getList(1, 20, {
+            filter: `title_eng ~ '${searchText}' || title ~ '${searchText}'`,
+            sort: "visite",
+        });
+        searchResults = resultList.items.map((item) => {
+            return item as unknown as Anime;
+        });
+    }
+
+    $: if (searchText.length >= 3) {
+        searchAnime();
+    } else {
+        searchResults = [];
+    }
 
     function toggleUserPreference(name: string, defaultValue: boolean = true) {
         const preference = localStorage.getItem("user_settings");
@@ -58,7 +81,31 @@
                     >Mau</a
                 >
             </div>
-            <div class="flex-none" />
+            <div class="flex-none">
+                <button
+                    class="btn btn-ghost btn-circle"
+                    on:click={() => {
+                        // @ts-ignore
+                        my_modal_search.showModal();
+                        searchText = "";
+                        searchResults = [];
+                    }}
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-5 w-5"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        ><path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                        /></svg
+                    >
+                </button>
+            </div>
         </div>
 
         <div class="container mx-auto mt-6 p-4 md:p-2">
@@ -99,7 +146,7 @@
             </li>
 
             <h2 class="text-2xl font-bold my-6">Settings</h2>
-            
+
             <li class="form-control">
                 <label
                     class="cursor-pointer label flex justify-start gap-8 p-4"
@@ -115,4 +162,44 @@
             </li>
         </ul>
     </div>
+
+    <dialog id="my_modal_search" class="modal">
+        <form method="dialog" class="modal-box w-fit max-w-none">
+            <div class="flex flex-col justify-center items-center">
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Search..."
+                        class="input input-bordered w-full max-w-xs"
+                        bind:value={searchText}
+                    />
+                </div>
+                {#if searchResults.length > 0}
+                    <div
+                        class="mt-6 h-[60vh] md:max-w-[80vw] md:min-w-[40vw] flex flex-wrap justify-center gap-4 md:gap-8 pb-10 overflow-y-auto"
+                    >
+                        {#each searchResults as result}
+                            <a
+                                class="card w-36 md:w-52 max-h-72 bg-base-100 shadow-xl image-full"
+                                href={`${base}/player/${result.slug}/1`}
+                                target="_self"
+                            >
+                                <figure>
+                                    <img src={result.imageurl} alt="Cover" class="w-full" />
+                                </figure>
+                                <div class="card-body w-36 md:w-52">
+                                    <h2 class="card-title">
+                                        {result.title_eng}
+                                    </h2>
+                                </div>
+                            </a>
+                        {/each}
+                    </div>
+                {/if}
+            </div>
+        </form>
+        <form method="dialog" class="modal-backdrop">
+            <button class="border-transparent focus:outline-none">close</button>
+        </form>
+    </dialog>
 </div>
