52a46eb5ed652a96d37a66e669725122226052a8
Simone <xsimone97@hotmail.it>
2024-02-14 23:09:03 +0100

    fix duplicate episodes

--- src/routes/player/[anime]/[episode]/+page.ts
+++ src/routes/player/[anime]/[episode]/+page.ts
@@ -6,10 +6,24 @@ export const load = (async ({params}) => {
     const { anime, episode } = params;
     const pb = new PocketBase("https://dev.opentrust.it/");
 
-    const resultList = await pb.collection('mau_episodes').getFullList({
+    let resultList = await pb.collection('mau_episodes').getFullList({
         filter: `(anime.slug="${anime}")`,
         expand: "anime",
     });
+
+    // if there are two episodes with the same number
+    // remove the one with the lower id
+    for (let i = 0; i < resultList.length; i++) {
+        for (let j = i + 1; j < resultList.length; j++) {
+            if (resultList[i].number === resultList[j].number) {
+                if (resultList[i].mau_id < resultList[j].mau_id) {
+                    resultList.splice(i, 1);
+                } else {
+                    resultList.splice(j, 1);
+                }
+            }
+        }
+    }
   
     return { "episodes": resultList as unknown as Episode[] };
   }) satisfies PageLoad;
