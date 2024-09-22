62022aee53459a84f8d3671fb1b9dc538e4fef6f
Simone <xsimone97@hotmail.it>
2024-09-22 22:06:59 +0200

    added posthog

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,19 @@
 // This can be false if you're using a fallback (i.e. SPA mode)
 export const prerender = true;
+
+import posthog from 'posthog-js'
+import { browser } from '$app/environment';
+
+export const load = async () => {
+
+  if (browser) {
+    posthog.init(
+      'phc_ECDHadrRNwpqT7tgukzWXjdazpw75B8hyA1Z7BhGXE8',
+      { 
+        api_host: 'https://eu.i.posthog.com',
+        person_profiles: 'always',
+      }
+    )
+  }
+  return
+};
